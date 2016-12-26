673175004319df59d3f477ef2870d56340bc2458
Sven Kommers <sven@svenkommers.nl>
2016-12-26 15:45:36 +0100

    solved

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
+$.ajax({
+  url: 'https://www.codeschool.com/users/skommers.json',
+  dataType: 'jsonp',
+  success: function(response){
+    console.log(response.courses.completed)
+    addCources(response.courses.completed)
+    //for completed in response.courses.completed {
 
-  // your code will go here
+
+    //}
+  }
+
+})
+function addCources(courses){
+  console.log("ran");
+var $badges = $('#badges');
+courses.forEach(function(course){
+  console.log('loop');
+  var $course = $('<div />',{
+    'class':'course'
+}).appendTo($badges)
+
+  $('<h3 />',{
+    text: course.title
+  }).appendTo($course)
+
+  $('<img />',{
+    src: course.badge
+  }).appendTo($course)
+
+  $('<a />',{
+    'class': 'bnt btn-primary',
+    target: '_blank',
+    href: course.url,
+    text: "see course"
+  }).appendTo($course)
+})
+}
 
 });
